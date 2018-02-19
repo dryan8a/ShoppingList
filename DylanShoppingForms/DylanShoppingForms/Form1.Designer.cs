7e0c9fc138f963c3736705a9d9c640b911c5b819
Dylan Ochoa <dryan8a@gmail.com>
2018-02-18 16:31:39 -0800

    Added adding, and started removing.

--- DylanShoppingForms/DylanShoppingForms/Form1.Designer.cs
+++ DylanShoppingForms/DylanShoppingForms/Form1.Designer.cs
@@ -28,19 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addItem = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.AddButton = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.removeItem = new System.Windows.Forms.TextBox();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // textBox2
+            // addItem
             // 
-            this.textBox2.Location = new System.Drawing.Point(86, 149);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
+            this.addItem.Location = new System.Drawing.Point(86, 149);
+            this.addItem.Name = "addItem";
+            this.addItem.Size = new System.Drawing.Size(100, 20);
+            this.addItem.TabIndex = 1;
             // 
             // label1
             // 
@@ -51,21 +54,15 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Item Name";
             // 
-            // button1
+            // AddButton
             // 
-            this.button1.Location = new System.Drawing.Point(86, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Add ";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(396, 90);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(207, 358);
-            this.treeView1.TabIndex = 6;
+            this.AddButton.Location = new System.Drawing.Point(86, 186);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add ";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // label3
             // 
@@ -76,16 +73,53 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "List";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Item Name";
+            // 
+            // removeItem
+            // 
+            this.removeItem.Location = new System.Drawing.Point(86, 247);
+            this.removeItem.Name = "removeItem";
+            this.removeItem.Size = new System.Drawing.Size(100, 20);
+            this.removeItem.TabIndex = 9;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(86, 290);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.RemoveButton.TabIndex = 10;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(396, 78);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(211, 394);
+            this.ListBox.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 487);
+            this.ClientSize = new System.Drawing.Size(619, 487);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.removeItem);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddButton);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.addItem);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -94,11 +128,14 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox addItem;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox removeItem;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.ListBox ListBox;
     }
 }
 
--- DylanShoppingForms/DylanShoppingForms/Form1.cs
+++ DylanShoppingForms/DylanShoppingForms/Form1.cs
@@ -16,5 +16,47 @@ namespace DylanShoppingForms
         {
             InitializeComponent();
         }
+        string[] list = new string[0];
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            string item = addItem.Text.Trim();
+            string[] temp = new string[list.Length + 1];
+            for (int i = 0; i < list.Length; i++)
+            {
+                temp[i] = list[i];
+            }
+            temp[temp.Length - 1] = item;
+            list = temp;
+            SetListBox();
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            string[] temp = new string[list.Length - 1];
+            int tempI = 0;
+            for(int i = 0; i<temp.Length; i++)
+            {
+                if(list[i] == removeItem.Text)
+                {
+                    tempI--;
+                }
+                else
+                {
+                    temp[tempI] = list[i];
+                }
+                tempI++;
+            }
+            list = temp;
+            SetListBox();
+        }
+
+        public void SetListBox()
+        {
+            ListBox.Items.Clear();
+            for (int i = 0; i < list.Length; i++)
+            {
+                ListBox.Items.Add(list[i]);
+            }
+        }
     }
 }
